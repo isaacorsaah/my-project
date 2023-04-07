9fce1277337fe61c9634545ca447d6ab4c993c6d
Isaac Orsaah <66434358+isaacorsaah@users.noreply.github.com>
2023-04-07 16:45:14 +0100

    Install authentication libraries

--- mersus/src/app/app.module.ts
+++ mersus/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminDashboardComponent,
+    EmployeeDashboardComponent
   ],
   imports: [
     BrowserModule,
